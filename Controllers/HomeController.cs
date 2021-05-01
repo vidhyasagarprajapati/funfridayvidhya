eac8a3ef0cd4f4f15dbbf8f8c5aa4b2ffaf5ba80
kukku981 <65335318+kukku981@users.noreply.github.com>
2021-05-01 13:32:42 +0530

    welcome to funfridayinube

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FunFriday.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string a, string b, string c, string d, string e, string f, string g, string h)
+        {
+            if (a == "a")
+            {
+                TempData["AlertMessage"] = "Sing " +
+                    "a song ";
+            }
+            if (b == "b")
+            {
+                TempData["AlertMessage"] = "How does function breakup ?";
+            }
+            if (c == "d")
+            {
+                TempData["AlertMessage"] = "What celebrity would you rate as a perfect 10 ?";
+            }
+            if (e == "e")
+            {
+                TempData["AlertMessage"] = " What is the best and worst purchases you’ve ever made?";
+            }
+            if (f == "f")
+            {
+                TempData["AlertMessage"] = "If you had to change your name, what would your new name be ?";
+            }
+            if (g == "g")
+            {
+                TempData["AlertMessage"] = " What is the best and worst purchases you’ve ever made?";
+            }
+            if (h == "h")
+            {
+                TempData["AlertMessage"] = " What is the best and worst purchases you’ve ever made?";
+            }
+            if (h == "h")
+            {
+                TempData["AlertMessage"] = " What is the best and worst purchases you’ve ever made?";
+            }
+            if (h == "h")
+            {
+                TempData["AlertMessage"] = " What is the best and worst purchases you’ve ever made?";
+            }
+            if (h == "h")
+            {
+                TempData["AlertMessage"] = " What is the best and worst purchases you’ve ever made?";
+            }
+            if (h == "h")
+            {
+                TempData["AlertMessage"] = " What is the best and worst purchases you’ve ever made?";
+            }
+            if (h == "h")
+            {
+                TempData["AlertMessage"] = " What is the best and worst purchases you’ve ever made?";
+            }
+
+            return View();
+        }
+    }
+}
